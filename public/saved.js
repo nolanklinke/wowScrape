9b9d8d92caf2378d7f80cea58fdb41ad5e5aa6cd
disconaked <nolan.klinke@gmail.com>
2018-09-20 16:29:41 -0400

    added basic note functionality

--- public/saved.js
+++ public/saved.js
@@ -26,3 +26,56 @@ $(document).on("click", "#btn-delete", function() {
     });
   });
 
+//modal on note button click
+$(document).on("click", ".btn-note", function() {
+  
+    $(".modal-title").empty();
+    $(".input").empty();
+  
+    // Save the id from .btn-note
+    var thisId = $(this).attr("data-id");
+  
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+      // With that done, add the note information to the page
+      .done(function(data) {
+        console.log(data);
+  
+        $(".modal-title").append("<h5>" + data.title + "</h5>");
+        $(".input").append("<textarea id='bodyinput' name='body'></textarea>");
+        $(".input").append("<button data-id='" + data._id + "' id='savenote' class='btn btn-primary btn-sm' style='margin-top:20px;'data-dismiss='modal'>Save Note</button>");
+  
+        // If there's a note in the article
+        if (data.note) {
+          // Place the body of the note in the body textarea
+          $("#bodyinput").val(data.note.body);
+        }
+      });
+  });
+
+$(document).on("click", "#savenote", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+  
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
+    
+      .done(function(data) {
+        // Log the response
+        console.log(data);
+        
+      });
+  
+    // Remove the values entered in the input and textarea for note entry
+    $("#bodyinput").val("");
+  });
+
